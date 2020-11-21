50440f34dcc11f99e3308d0e71cd37d07e7cad3d
Jennifer Huynh <jntlhuynh@gmail.com>
2020-11-20 21:11:28 -0800

    linked css with html and added comments to scripts

---
+++ script.js
@@ -0,0 +1,10 @@
+// create 16x16 grid of square divs
+// use another container div
+// look into float/clear, inline-block, flexbox, CSS grid
+// careful with borders and margins
+
+// set up hover effect when mouse enters a div 
+// and ends when mouse leaves it
+// set up event listeners for either of those
+
+// add new class to div to change color
