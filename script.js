761550cf8e9cccad1798f391677e5696ae9ec36d
Jennifer Huynh <jntlhuynh@gmail.com>
2020-11-23 22:58:41 -0800

    created a small grid to text out css grid

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 // create 16x16 grid of square divs
 // use another container div
+const container = document.querySelector(".container");
+console.log(container);
 // look into float/clear, inline-block, flexbox, CSS grid
+// USING CSS GRID
 // careful with borders and margins
 
 // set up hover effect when mouse enters a div 
