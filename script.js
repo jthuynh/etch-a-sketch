ee43dcfa41d28bdaa47a7df98176d8e8795e6f34
Jennifer Huynh <jntlhuynh@gmail.com>
2020-12-07 01:39:56 -0800

    removed padding in stylesheet that was messing with the aspect ratio of the grid

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // create 16x16 grid of square divs
 // use another container div
-const container = document.querySelector(".container");
+const container = document.querySelector(".grid-container");
 console.log(container);
 // look into float/clear, inline-block, flexbox, CSS grid
 // USING CSS GRID
@@ -12,3 +12,17 @@ console.log(container);
 
 // add new class to div to change color
 
+function startGame(gridNumber) {
+    document.getElementById("grid-container").style.setProperty('grid-template-columns', `repeat(${gridNumber}, 1fr)`);
+    document.getElementById("grid-container").style.setProperty('grid-template-rows', `repeat(${gridNumber}, 1fr)`);
+
+
+
+    for(x = 0; x < gridNumber * gridNumber; x++) {
+        var div = document.createElement('div');
+        
+        document.getElementById('grid-container').appendChild(div);
+    }
+}
+
+startGame(25);
